552f045fb743f9be13820e53ac0b59d12ecdc014
Majevsong <mucahit.soylemez42@gmail.com>
2025-09-18 23:12:13 +0300

    version 1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://yusufoztekin.github.io/yusufoztur',
+  base: '/yusufoztur',
   server: { port: 4321 },
   vite: {
     server: { fs: { strict: false } },
